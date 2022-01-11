bacf53f5dcae66d793b1307eba09ac31b0caf73b
Adam Roberts <Adam_roberts@gmx.com>
2022-01-11 17:56:00 -0500

    UI done

--- RGB Hex Code Converter/ViewController.swift
+++ RGB Hex Code Converter/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redTextField: UITextField!
+    
+    @IBOutlet weak var greenTextField: UITextField!
+    
+    @IBOutlet weak var blueTextField: UITextField!
+    
+    @IBOutlet weak var alphaTextField: UITextField!
+    
+    @IBOutlet weak var conversionButton: UIButton!
+    
+    
+    @IBOutlet weak var resultsLsbel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func selectSegmentAction(_ sender: UISegmentedControl) {
+    }
+    
+    @IBAction func convertAction(_ sender: UIButton) {
+    }
+    
 
 }
 
