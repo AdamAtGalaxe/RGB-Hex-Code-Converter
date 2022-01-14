aa8806b5ba106decc6549798cb9ff8049da07e6b
Adam Roberts <Adam_roberts@gmx.com>
2022-01-14 14:24:40 -0500

    minor changes

--- RGB Hex Code Converter/ViewController.swift
+++ RGB Hex Code Converter/ViewController.swift
@@ -10,17 +10,18 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var redTextField: UITextField!
-    
     @IBOutlet weak var greenTextField: UITextField!
-    
     @IBOutlet weak var blueTextField: UITextField!
-    
     @IBOutlet weak var alphaTextField: UITextField!
-    
     @IBOutlet weak var conversionButton: UIButton!
-    
-    
     @IBOutlet weak var resultsLsbel: UILabel!
+    
+    enum Conversion {
+        case hexToRGB
+        case RGBToHex
+    }
+    
+    var conversion : Conversion = .hexToRGB
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,11 +29,33 @@ class ViewController: UIViewController {
     }
     
     @IBAction func selectSegmentAction(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex{
+        case 0 :
+            conversion = .hexToRGB
+            conversionButton.setTitle("Convert hex to RGB", for: .normal)
+        case 1:
+            conversion = .RGBToHex
+            conversionButton.setTitle("Convert RGB to hx", for: .normal)
+        default:
+            conversion = .hexToRGB
+            conversionButton.setTitle("Convert hex to RGB", for: .normal)
+        }
+        print(conversion)
+        redTextField.text?.removeAll()
+        blueTextField.text?.removeAll()
+        greenTextField.text?.removeAll()
+        
     }
     
     @IBAction func convertAction(_ sender: UIButton) {
+        
     }
     
+    func convertHexToRGB(){
+        
+    }
+    func convertRGBToHex(){
+        
+    }
 
 }
-
