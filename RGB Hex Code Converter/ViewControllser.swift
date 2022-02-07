ff9bac4b87147eaa19447fc46938dabcc3502e40
Adam Roberts <Adam_roberts@gmx.com>
2022-02-07 09:45:21 -0500

    only needs minor changes

--- RGB Hex Code Converter/ViewControllser.swift
+++ RGB Hex Code Converter/ViewControllser.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var blueTextField: UITextField!
     @IBOutlet weak var alphaTextField: UITextField!
     @IBOutlet weak var conversionButton: UIButton!
-    @IBOutlet weak var resultsLsbel: UILabel!
+    @IBOutlet weak var resultsLabel: UILabel!
     
     @IBOutlet weak var colorView: UIView!
     enum Conversion {
@@ -53,9 +53,11 @@ class ViewController: UIViewController {
 
         switch conversion {
         case .hexToRGB:
-            colorView.backgroundColor = convertHexToRGB()
+            view.backgroundColor = convertHexToRGB()
+            //colorView.backgroundColor = convertHexToRGB()
         case .RGBToHex:
-            colorView.backgroundColor = convertRGBToHex()
+            //colorView.backgroundColor = convertRGBToHex()
+            view.backgroundColor = convertRGBToHex()
         }
         
     }
@@ -69,32 +71,73 @@ class ViewController: UIViewController {
         
         guard let alphaText = Float(alphaTextField.text!) else{ return nil  }
         if(alphaText>1 || alphaText<0) { return nil}
+        
+        let red : CGFloat = CGFloat(redText)/255.0
+        let green : CGFloat  = CGFloat(greenText)/255.0
+        let blue : CGFloat  = CGFloat(blueText)/255.0
 
         
-        resultsLsbel.text = "Red: \(redText)\nGreen: \(greenText)\nBlue: \(blueText)\nAlpha: \(alphaText)"
-        let backgroundColor = UIColor(displayP3Red: CGFloat(redText), green: CGFloat(greenText), blue: CGFloat(blueText), alpha: CGFloat(alphaText))
+        resultsLabel.text = "Red: \(redText)\nGreen: \(greenText)\nBlue: \(blueText)\nAlpha: \(alphaText)"
+        
+        let backgroundColor = UIColor(red: CGFloat(redText)/255.0, green: CGFloat(greenText)/255.0, blue: CGFloat(blueText)/255.0, alpha: CGFloat(alphaText))
+        
+        resultsLabel.backgroundColor = UIColor(red: getLabelColor(red), green: getLabelColor(green), blue: getLabelColor(blue), alpha: 1)
         
         return backgroundColor
     }
     func convertRGBToHex()->UIColor?{
-        guard let redText = UInt8(redTextField.text!) else{ return nil }
+        guard let redText = UInt8(redTextField.text!) else{ return UIColor.white }
         
-        guard let greenText = UInt8(greenTextField.text!) else{ return nil}
+        guard let greenText = UInt8(greenTextField.text!) else{ return UIColor.white}
         
-        guard let blueText = UInt8(blueTextField.text!) else{ return nil}
+        guard let blueText = UInt8(blueTextField.text!) else{ return UIColor.white}
         
-        guard let alphaText = Float(alphaTextField.text!) else{ return nil  }
+        guard let alphaText = Float(alphaTextField.text!) else{ return UIColor.white  }
         if(alphaText>1 || alphaText<0) { return nil}
         
-        let redString = String(format: "%2X", redText)
-        let greenString = String(format: "%2X", greenText)
-        let blueString = String(format: "%2X", blueText)
+        let red : CGFloat = CGFloat(redText)/255.0
+        let green : CGFloat  = CGFloat(greenText)/255.0
+        let blue : CGFloat  = CGFloat(blueText)/255.0
         
-        resultsLsbel.text = "HexCode: #\(redString)\(greenString)\(blueString)\nAlpha: \(alphaText)"
-        let backgroundColor = UIColor(displayP3Red: CGFloat(redText), green: CGFloat(greenText), blue: CGFloat(blueText), alpha: CGFloat(alphaText))
+        let redString = String(format: "%02X", redText)
+        let greenString = String(format: "%02X", greenText)
+        let blueString = String(format: "%02X", blueText)
         
-        return backgroundColor
+        if(red+green+blue>0.80){
+            resultsLabel.textColor = .black
+        }
+        else{
+            resultsLabel.textColor = .white
+        }
+        //let test : String = (pad: greenString, toWidth: 3, using: "0")
+        resultsLabel.text = "HexCode: #\(redString)\(greenString)\(blueString)\nAlpha: \(alphaText)"
+        
+        let myBackgroundColor = UIColor(red: red, green: green, blue: blue, alpha: CGFloat(alphaText))
+        
+        resultsLabel.backgroundColor = UIColor(red: getLabelColor(red), green: getLabelColor(green), blue: getLabelColor(blue), alpha: 1)
+ 
+        //resultsLsbel.textColor = .white
+
+    
+            
+        return myBackgroundColor
         
     }
+    func getLabelColor(_ color: CGFloat) ->CGFloat {
+        print(color)
+        //if color == 0.0 { return 0 }
+         if color <= 0.2{
+            return color+0.15
+        }
+        else if color > 0.2 && color <= 0.50 {
+            return color+(color*0.15)
+        }
+        else if color > 0.5 && color < 0.8 {
+            return color-(color*0.15)
+        }
+        else { return color-0.15}
+            
+    }
+    
 
 }
